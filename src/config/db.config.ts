1f9eed921230c07599ff5c2332d44a4cac060800
clevereziogor@gmail.com <clevereziogor@gmail.com>
2021-09-24 11:05:11 +0100

    fic

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,7 +2,7 @@ export const Config = {
   HOST: "localhost",
   USER: "mac",
   PASSWORD: '',
-  DB: 'postgres://ngkuqkpnqfyavk:d7fcfe79d79524617d020c56f5561b89a115113da342886eb2ebb686f9056a7f@ec2-34-203-155-237.compute-1.amazonaws.com:5432/d56hvhd6g1jc4o' || "metacaredata",
+  DB: 'postgres://ngkuqkpnqfyavk:d7fcfe79d79524617d020c56f5561b89a115113da342886eb2ebb686f9056a7f@ec2-34-203-155-237.compute-1.amazonaws.com:5432/d56hvhd6g1jc4o',
   dialect: "postgres",
   use_env_variable: "DATABASE_URL"
 };
