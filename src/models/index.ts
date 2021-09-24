0d1b215e3573a108763169f10792336a3a8cfada
clevereziogor@gmail.com <clevereziogor@gmail.com>
2021-09-24 11:10:23 +0100

    fix

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,8 +6,7 @@ import {Sequelize} from "sequelize-typescript";
 const sequelize = new Sequelize(Config.DB, Config.USER, Config.PASSWORD, {
   host: Config.HOST,
   dialect: "postgres",
-  // operatorsAliases: false,
-  // models: (__dirname + '/models')
+  
 });
 
 const db: Record<string, any> = {};
